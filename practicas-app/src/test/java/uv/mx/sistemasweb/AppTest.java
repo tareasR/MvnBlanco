96a8c900dff90be474908ba7dbca6cf2338fd877
tareasR <tareasrojano@gmail.com>
2020-10-21 19:29:17 -0500

    Inicial commit de un proyecto en blanco
    El proyecto se creó con maven mediante el siguiente comando:
    mvn archetype:generate -DgroupId=uv.mx.sistemasweb  -DartifactId=practicas-app -DarchetypeArtifactId=maven-archetype-quickstart  -DarchetypeVersion=1.4 -DinteractiveMode=false

---
+++ practicas-app/src/test/java/uv/mx/sistemasweb/AppTest.java
@@ -0,0 +1,20 @@
+package uv.mx.sistemasweb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
